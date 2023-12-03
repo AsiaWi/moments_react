9c68526d03e7496eebf2f90a7172fd943abb27b4
AsiaWi <joannawitek@icloud.com>
2023-12-03 21:50:07 +0000

    Navbar work complete

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://moments-react-asia-493c23a37d81.herokuapp.com/'
+axios.defaults.baseURL = 'https://django-rest-frmwrk-9285b10668e7.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
